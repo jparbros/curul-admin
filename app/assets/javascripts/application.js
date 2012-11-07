d88fe82c8e97e23197338fddd7f26dd7d3a8d61a
Jorge Pardiñas <jorge.parbros@parbros.net>
2012-11-07 09:27:29 -0600

    agregando filtrado por valores vacios

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,64 @@
 //= require bootstrap-datepicker
 //= require jquery.flot.min
 //= require jquery.flot.time
+
+$(document).ready(function() {
+  $('.leftmenu a').click(function(e){
+      if($(this).siblings('ul').size() == 1){
+        e.preventDefault();
+          var submenu = $(this).siblings('ul');
+          if($(this).hasClass('open')) {
+              if($(this).parents('.leftmenu').hasClass('lefticon')) {
+                  submenu.fadeOut();
+              } else {
+                  submenu.slideUp('fast');
+              }
+              $(this).removeClass('open');
+          } else {
+              if($(this).parents('.leftmenu').hasClass('lefticon')) {
+                  submenu.fadeIn();
+              } else {
+                  submenu.slideDown('fast');
+              }
+              $(this).addClass('open');
+          }
+      }
+  })
+});
+
+$(document).ready(function(){
+  $(".alert-message .close").click(function(){
+    $(".alert-message").alert('close');
+  });
+
+  $("#initiative_topic_tokens").tokenInput("/topics.json", {
+      theme: "facebook",
+      prePopulate: $("#initiative_topic_tokens").data("pre"),
+      crossDomain: false
+  });
+
+  $("#initiative_presented_by_token").tokenInput("/representatives.json", {
+      theme: "facebook",
+      prePopulate: $("#initiative_presented_by_token").data("pre"),
+      crossDomain: false
+  });
+
+  $("#representative_commission_tokens").tokenInput("/commissions.json", {
+      theme: "facebook",
+      prePopulate: $("#representative_commission_tokens").data("pre"),
+      crossDomain: false
+  });
+
+  $("#initiative_commission_tokens").tokenInput("/commissions.json", {
+      theme: "facebook",
+      prePopulate: $("#initiative_commission_tokens").data("pre"),
+      crossDomain: false
+  });
+  
+  $("#search_commission_ids").tokenInput("/commissions.json", {
+      theme: "facebook",
+      crossDomain: false
+  });
+  
+  $('#loader').ajaxStart(function() { $(this).show(); }).ajaxStop( function() { $(this).hide(); })
+});
